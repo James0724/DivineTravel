44c2831e2e3b24ee8743c96ca23e02454521cd0f
James0724 <jameskahoro07@gmail.com>
2026-06-14 17:01:23 +0300

    refactor: add descriptive comment for heading character pull-up in IntroSection

--- components/home/IntroSection.tsx
+++ components/home/IntroSection.tsx
@@ -43,6 +43,7 @@ export default function IntroSection() {
               </div>
             </Reveal>
 
+            {/* Heading — character pull-up */}
             <AnimatedHeading
               as="h1"
               textBefore="Your trusted gateway to "
