80f14636a58c274a22c415c0b045e645d124a576
James Ndara <82360313+ndaran@users.noreply.github.com>
2026-08-07 11:33:02 +0300

    Update Hero.tsx

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -12,7 +12,7 @@ const EASE: [number, number, number, number] = [0.22, 1, 0.36, 1];
 
 const slides = [
   {
-    src: "https://res.cloudinary.com/dk2j3k15k/image/upload/c_fill,ar_16:9,f_auto,q_auto/v1784902109/Gallarey/herosection_biel7p.jpg",
+    src: "https://res.cloudinary.com/qgyno6bc/image/upload/v1786091557/CLIFF_STILLS15_yalxqf.jpg",
     alt: "A trip to the masai mara national reserve in Kenya, Africa",
   },
   {
